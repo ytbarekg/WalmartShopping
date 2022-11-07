da28240f9c817e8859b28ab6bb3f42b1d9317275
ytbarekg <yhailu@miu.edu>
2022-11-06 22:39:18 -0800

    initial commit

---
+++ app/src/main/java/edu/miu/mdp/walmartshopping/DetailActivity.kt
@@ -0,0 +1,11 @@
+package edu.miu.mdp.walmartshopping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
